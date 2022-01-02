60155dbcf37fe20bd240e893d0d71eaf9daa0ac4
DenisPedersen <89845900+DenisPedersen@users.noreply.github.com>
2022-01-02 13:22:06 +0100

    Add files via upload

---
+++ Cheatsheet/src/DataStruktur/Stack.java
@@ -0,0 +1,14 @@
+package DataStruktur;
+
+//LIFO Last in first out - sidst ind, først ud.
+//Stack skal ses som en bunke tallerkner, hvor man kan stable data og fjerne data.
+
+public class Stack {
+
+
+
+    //size(); størrelsen på hele stakken
+    //push(values); tilføjer data til stakken
+    //pop; fjerner elementer fra stakken
+
+}
